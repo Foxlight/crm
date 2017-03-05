81f92b070ddc60e295f62d28a63a1eb4d89779c0
Florian Champoussin <melody@macbook-pro-de-melody.home>
2017-03-05 21:04:29 +0100

    Maintenant c’est joli

--- CRM2016/CRM2016-ejb/src/java/sessions/EntrepriseFacade.java
+++ CRM2016/CRM2016-ejb/src/java/sessions/EntrepriseFacade.java
@@ -39,32 +39,32 @@ public class EntrepriseFacade extends AbstractFacade<Entreprise> {
     public void creerEntreprisesDeTest() {
         System.out.println("CREATION DE DONNEES DE TEST");
 
-        Entreprise e1 = new Entreprise("Miage Nice",
-                "Route des Lucioles",
+        Entreprise e1 = new Entreprise("Amadeus",
+                "485 route du pin montard",
                 "06560", "Sophia-Antipolis",
-                "La meilleure entreprise du monde");
+                "Spécialisé dans la création de sites pour airlines");
 
         // utilisation de la méthode héritée pour faire
         // un insert
         create(e1);
         // une interaction pour l'entreprise
-        Interaction i1 = new Interaction("Appel de Mr Tartanpion pour la taxe d'aprentissage au 0654345323", InteractionType.TELEPHONE);
+        Interaction i1 = new Interaction("Appel de Mr Piccinini pour la taxe d'aprentissage au 0654345323", InteractionType.TELEPHONE);
         // on fait un insert dans la table des interactions
 
         interactionFacade.create(i1);
         // on met à jour la jointure
         e1.addInteraction(i1);
 
-        Interaction i2 = new Interaction("Email à Mr Dupont", InteractionType.EMAIL);
+        Interaction i2 = new Interaction("Email à Mme Henriet", InteractionType.EMAIL);
         // on fait un insert dans la table des interactions
         interactionFacade.create(i2);
         // on met à jour la jointure
         e1.addInteraction(i2);
 
-        Entreprise e2 = new Entreprise("Polytech Nice",
-                "Site des Templier",
-                "06560", "Sophia-Antipolis",
-                "De l'autre côté de la rue");
+        Entreprise e2 = new Entreprise("La langue du caméléon",
+                "1 place du pin",
+                "06300", "Nice",
+                "La meilleure agence web");
 
         // utilisation de la méthode héritée pour faire
         // un insert
@@ -78,25 +78,25 @@ public class EntrepriseFacade extends AbstractFacade<Entreprise> {
 
         Stage s = new Stage();
         s.setAdministrateur(a);
-        s.setContenu("pouuuuuuuuuuuuueeeeeeeeeeeeetttttttttttt");
+        s.setContenu("Stage de développement web");
         e2.addInteraction(s);
 
         Etudiant etudiant1 = new Etudiant("Pierre", "Puybonnieux", 21103671);
         etudiantFacade.create(etudiant1);
 
-        Etudiant etudiant11 = new Etudiant("Pierre1", "Puybonnieux1", 121103671);
+        Etudiant etudiant11 = new Etudiant("Florian", "Champoussin", 20905070);
         etudiantFacade.create(etudiant11);
-        Etudiant etudiant12 = new Etudiant("Pierre2", "Puybonnieux2", 221103671);
+        Etudiant etudiant12 = new Etudiant("Lorris", "Spugna", 22110367);
         etudiantFacade.create(etudiant12);
-        Etudiant etudiant13 = new Etudiant("Pierre3", "Puybonnieux3", 321103671);
+        Etudiant etudiant13 = new Etudiant("David", "Boccara", 33874627);
         etudiantFacade.create(etudiant13);
-        Etudiant etudiant14 = new Etudiant("Pierre4", "Puybonnieux4", 421103671);
+        Etudiant etudiant14 = new Etudiant("Morgane", "Demeocq", 48493748);
         etudiantFacade.create(etudiant14);
-        Etudiant etudiant15 = new Etudiant("Pierre5", "Puybonnieux5", 521103671);
+        Etudiant etudiant15 = new Etudiant("Bruno", "Lazartigues", 54773849);
         etudiantFacade.create(etudiant15);
-        Etudiant etudiant16 = new Etudiant("Pierre6", "Puybonnieux6", 621103671);
+        Etudiant etudiant16 = new Etudiant("Annie", "Dupont", 63746573);
         etudiantFacade.create(etudiant16);
-        Etudiant etudiant17 = new Etudiant("Pierre7", "Puybonnieux7", 721103671);
+        Etudiant etudiant17 = new Etudiant("John", "Rambo", 73847382);
         etudiantFacade.create(etudiant17);
 
     }
